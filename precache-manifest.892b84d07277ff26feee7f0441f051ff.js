142c279311d80bf84dff991bf9bd2a610ab6b833
Akhil <Akhil@AKHIL-PC.com>
2020-06-11 13:00:26 +0400

    Updates

---
+++ precache-manifest.892b84d07277ff26feee7f0441f051ff.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44a39a1d69b1c651e7da70c2b30e97e",
+    "url": "/FoodSearch/index.html"
+  },
+  {
+    "revision": "567ed07146131a5803b1",
+    "url": "/FoodSearch/static/css/main.eb855a32.chunk.css"
+  },
+  {
+    "revision": "c74a5d880827aab3eb72",
+    "url": "/FoodSearch/static/js/2.a3a843fe.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/FoodSearch/static/js/2.a3a843fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567ed07146131a5803b1",
+    "url": "/FoodSearch/static/js/main.fd90da82.chunk.js"
+  },
+  {
+    "revision": "be8be63f495a20d7641e",
+    "url": "/FoodSearch/static/js/runtime-main.69c5be4e.js"
+  },
+  {
+    "revision": "1e00d32e05c717312a7af4909158d867",
+    "url": "/FoodSearch/static/media/food1.1e00d32e.jpg"
+  },
+  {
+    "revision": "4340c0ed5a6c22731e5f48d9076dc925",
+    "url": "/FoodSearch/static/media/food2.4340c0ed.jpg"
+  },
+  {
+    "revision": "33a847c7c0aa11386cfb668c2b015b30",
+    "url": "/FoodSearch/static/media/food3.33a847c7.jpg"
+  },
+  {
+    "revision": "cd9e680851cd5f5fda84b92c645c402b",
+    "url": "/FoodSearch/static/media/food4.cd9e6808.jpeg"
+  },
+  {
+    "revision": "466ba41103244251c6991df44cd67adc",
+    "url": "/FoodSearch/static/media/food5.466ba411.jpg"
+  },
+  {
+    "revision": "954c8aaeb35e4833a75e24d763d22cbb",
+    "url": "/FoodSearch/static/media/food6.954c8aae.jpg"
+  },
+  {
+    "revision": "253a02c290e4196d0f3fa15140d028b0",
+    "url": "/FoodSearch/static/media/noResults-thinking.253a02c2.png"
+  },
+  {
+    "revision": "715ff2034941b627058d62513d9fb66e",
+    "url": "/FoodSearch/static/media/system-search.715ff203.png"
+  }
+]);
